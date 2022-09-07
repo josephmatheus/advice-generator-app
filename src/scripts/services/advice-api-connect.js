f658a7ecf4ec5dcdfa3325fc40da5515e15eee68
Joseph Matheus <josephmatheus@hotmail.com>
2022-09-07 12:02:15 -0300

    fix: adding parameter to the fetch request so that the request is not cached, which made the button to generate new request not work in some browsers like Firefox

--- src/scripts/services/advice-api-connect.js
+++ src/scripts/services/advice-api-connect.js
@@ -1,6 +1,6 @@
 async function adviceApiConnect(){
     const url = 'https://api.adviceslip.com/advice'
-    const response = await fetch(url)
+    const response = await fetch(url, {cache: "no-store"})
     return await response.json()
 }
 
